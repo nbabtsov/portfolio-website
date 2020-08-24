82ff79c18aeb54358a5c3d8bc8b96f7957f8bdb7
Nika Babtsov <nikababtsov12@gmail.com>
2020-08-24 13:04:48 -0500

    animation added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Matrix} from './components/woo';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hi, I'm Nika</p>
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>Hi, I'm Nika. </p>*/}
       </header>
+     <Matrix style={{backgroundColor: 'blue'}} />
+
     </div>
   );
 }
